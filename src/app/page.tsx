a5928b22e76fb5f0a4cf7980a20443402d4bdce0
tevada <116619526+PennDarath@users.noreply.github.com>
2023-11-28 12:24:52 +0700

    fixed tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
+import SecondBanner from "@/components/secondbanner/page";
 import { Text } from "@/constants/Sizes";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div
-        style={{
-          fontSize: Text.xs,
-        }}
-      >
-        esfwafe
+    <main className="flex min-h-screen flex-col bg-white items-center justify-between p-24">
+      <div>
+        <SecondBanner />
       </div>
     </main>
   );
